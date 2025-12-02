514a8c3725a3cf1373e6e928473bda01173763e2
abrahamgallio <abrahamgallio@gmail.com>
2025-12-02 15:00:23 -0300

    Update apps.py

--- appweb/apps.py
+++ appweb/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class AppwebConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'appweb'
+
+    def ready(self):
+        import appweb.signals  # ← Activa las señales
