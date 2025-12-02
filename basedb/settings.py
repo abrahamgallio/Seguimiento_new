22fefba257ecdd00e6f2354ffdbaa711f2c5bd7f
abrahamgallio <abrahamgallio@gmail.com>
2025-12-02 14:46:46 -0300

    Update settings.py

--- basedb/settings.py
+++ basedb/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-pt%$o(ou#o10ubqt*#w-fn%)*1twwj%ii1*884fu*vec-ojna_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
@@ -136,3 +137,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
